6f87d6089829d1752592963e3e2046bb0a28326f
Igor Gorbunov <igor@homepc.home.org>
2015-12-12 23:21:21 +0300

    Frontend driver for SPARK7162 from AR-P

--- frontends/spark7162/tuner/mxl_common.c
+++ frontends/spark7162/tuner/mxl_common.c
@@ -1,5 +1,7 @@
-#include <types.h>
+
+#include "ywdefs.h"
 #include "mxl_common.h"
+
 
 /* ========================== Local functions called by Init and RFTune ============================ */
 UINT32 SetIRVBit(PIRVType pIRV, UINT8 Num, UINT8 Mask, UINT8 Val)
@@ -9,10 +11,10 @@ UINT32 SetIRVBit(PIRVType pIRV, UINT8 Num, UINT8 Mask, UINT8 Val)
 	/* while (pIRV->Num != 0xFF && pIRV->Val != 0xFF)*/
 	while (pIRV->Num != 0xFF || pIRV->Val != 0xFF)
 	{
-		if (pIRV->Num == Num)
+		if (pIRV->Num==Num)
 		{
-			pIRV->Val &= ~Mask;
-			pIRV->Val |= Val;
+			pIRV->Val&=~Mask;
+			pIRV->Val|=Val;
 		}
 		pIRV++;
 
@@ -25,10 +27,10 @@ UINT32 SetIRVBit_Ext(PIRVType pIRV, UINT8 Num1, UINT8 Mask, UINT8 Val1, UINT8 Va
 {
 	while (pIRV->Num != 0xFF || pIRV->Val != 0xFF)
 	{
-		if (pIRV->Num == Num1 && pIRV->Val == Val1)
+		if (pIRV->Num==Num1 && pIRV->Val==Val1)
 		{
-			(pIRV + 1)->Val &= ~Mask;
-			(pIRV + 1)->Val |= Val2;
+			(pIRV+1)->Val&=~Mask;
+			(pIRV+1)->Val|=Val2;
 			return MxL_OK;
 		}
 		pIRV++;
@@ -41,41 +43,41 @@ UINT32 SetIRVBit_Ext(PIRVType pIRV, UINT8 Num1, UINT8 Mask, UINT8 Val1, UINT8 Va
    Removed to support integers only : hchan 27/02/2009 */
 /*SINT32 round_d(double inVal)
 {
-    double modulus = 0.0;
-    int intVal = (int)(inVal);
-    int retVal = 0;
-    modulus = inVal - intVal;
+	double modulus = 0.0;
+	int intVal = (int)(inVal);
+	int retVal = 0;
+	modulus = inVal - intVal;
 
-    if (modulus >= 0.5 && inVal >0)
-        retVal = (intVal + 1);
-    else if(modulus < 0.5 && inVal > 0)
-        retVal =  (intVal);
-    else if(-(modulus) >= 0.5 && inVal < 0)
-        retVal = (intVal - 1);
-    else if(-(modulus) < 0.5 && inVal < 0)
-        retVal = (intVal);
-    else retVal = (0);
+	if (modulus >= 0.5 && inVal >0)
+		retVal = (intVal + 1);
+	else if(modulus < 0.5 && inVal > 0)
+		retVal =  (intVal);
+	else if(-(modulus) >= 0.5 && inVal < 0)
+		retVal = (intVal - 1);
+	else if(-(modulus) < 0.5 && inVal < 0)
+		retVal = (intVal);
+	else retVal = (0);
 
-    return(retVal);
+	return(retVal);
 }
 */
 
 UINT32 div_rnd_uint32(UINT32 numerator, UINT32 denominator)
 {
-	UINT32 quotient = numerator / denominator;
-	UINT32 remainder = numerator % denominator;
+	UINT32 quotient = numerator/denominator;
+	UINT32 remainder = numerator%denominator;
 
 	/* round up based on teh remainder and denominator */
-	if (denominator % 2 == 1) /*an odd denominator */
+	if(denominator%2 == 1) /*an odd denominator */
 	{
-		if (remainder >= denominator / 2)
+		if(remainder >= denominator/2)
 			quotient++;
 	}
 	else /* and even denominator */
 	{
-		if (remainder > denominator / 2)
+		if(remainder > denominator/2)
 			quotient ++;
 	}
 
-	return (quotient);
+	return(quotient);
 }
